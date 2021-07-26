aa22e4377abe8ab49be5aa86567d6d62e8aad627
WojCzu <wojciech.czuma@gmail.com>
2021-07-27 00:13:16 +0200

    fetch api working properly

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 import "./sass/style.scss";
 
+const API_URL = "http://api.nbp.pl/api/exchangerates/rates";
+
 const appForm = document.querySelector(".app__form");
 const resultContainer = document.querySelector(".result");
 const amountInput = appForm.querySelector(".form__input--amount");
 const dateInput = appForm.querySelector(".form__input--date");
 const currencyInput = appForm.querySelector(".form__input--currency");
 
-const handleClick = e => {
+const handleSubmit = e => {
   e.preventDefault();
   const amount = amountInput.value;
   const date = dateInput.value;
   const currency = currencyInput.value;
-  getData(date, currency);
+  getData(date, currency).then(data => console.log(data));
   calculate(amount);
   showResult();
 };
 const getData = (date, currency) => {
-  return;
+  const startingDate = new Date(date);
+  const endingDate = new Date(date);
+  previousDate(endingDate, 1);
+  previousDate(startingDate, 8);
+  const formatStartingDate = formatDate(startingDate);
+  const formatEndingDate = formatDate(endingDate);
+  const api = `${API_URL}/A/${currency}/${formatStartingDate}/${formatEndingDate}`;
+
+  return fetch(api)
+    .then(res => res.json())
+    .then(({ code, rates }) => {
+      const {
+        no: table,
+        effectiveDate: date,
+        mid: exchangeRate,
+      } = rates.sort(rate => rate.effectiveDate).reverse()[0];
+      return { code, table, date, exchangeRate };
+    });
 };
+const previousDate = (date, days) => date.setDate(date.getDate() - days);
+const formatDate = date => date.toISOString().split("T")[0];
+
 const calculate = amount => {
   return;
 };
@@ -25,4 +47,4 @@ const showResult = () => {
   return;
 };
 
-appForm.addEventListener("submit", handleClick);
+appForm.addEventListener("submit", handleSubmit);
