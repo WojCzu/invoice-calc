0f176d18d5d7c5b246a909fe13d21cb915af0b91
WojCzu <wojciech.czuma@gmail.com>
2021-07-26 20:07:29 +0200

    add handle submit event - do nothing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./sass/style.scss";
+
+const appForm = document.querySelector(".app__form");
+const resultContainer = document.querySelector(".result");
+const amountInput = appForm.querySelector(".form__input--amount");
+const dateInput = appForm.querySelector(".form__input--date");
+const currencyInput = appForm.querySelector(".form__input--currency");
+
+const handleClick = e => {
+  e.preventDefault();
+  const amount = amountInput.value;
+  const date = dateInput.value;
+  const currency = currencyInput.value;
+  getData(date, currency);
+  calculate(amount);
+  showResult();
+};
+const getData = (date, currency) => {
+  return;
+};
+const calculate = amount => {
+  return;
+};
+const showResult = () => {
+  return;
+};
+
+appForm.addEventListener("submit", handleClick);
